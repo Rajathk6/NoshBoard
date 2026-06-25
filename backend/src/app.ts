34e284369b7a321fb6f98d87aed51188d49bd60e
rajath <rajathkumar120@gmail.com>
2026-06-25 17:04:57 +0530

    fix: cors policy error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,13 @@
 // backend/src/app.ts
 
 import express from "express";
+import cors from "cors";
 import dishRouter from "./routes/dish.routes";
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 
 app.use("/api", dishRouter);
