a62638e694da684cbaf2e44503dfb70b52a819ae
rajath <rajathkumar120@gmail.com>
2026-06-25 17:00:39 +0530

    feat: fetching dishes from backend

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
+import { getDishes } from "../api/api";
+import type { Dish } from "../types/dish";
+
 const Dashboard = () => {
+    const [dishes, setDishes] = useState<Dish[]>([]);
+
+    async function fetchDishes() {
+        try {
+            const response = await getDishes();
+            setDishes(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchDishes();
+    }, []);
+
     return (
         <div>
-            Dashboard
+            <h1>Nosh Dashboard</h1>
+
+            {dishes.map((dish) => (
+                <div key={dish.id}>
+                    {dish.dishName}
+                </div>
+            ))}
         </div>
-    )
-}
+    );
+};
 
 export default Dashboard;
